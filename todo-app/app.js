d1a3318c25db57453fae7e3faa5bfc8ad1241ada
Sridhar-89 <sridharbodhanapu89@gmail.com>
2023-05-14 11:21:57 +0530

    Add new todo

--- todo-app/app.js
+++ todo-app/app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 
@@ -58,8 +59,8 @@ app.get("/todos/:id", async function (request, response) {
 
 app.post("/todos", async function (request, response) {
   try {
-    const todo = await Todo.addTodo(request.body);
-    return response.json(todo);
+    await Todo.addTodo(request.body);
+    return response.redirect("/");
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
